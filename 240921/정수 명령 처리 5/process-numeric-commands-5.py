9bfd9ad305ec0921c9ca9f7d07daf55968007c7d
Jiyoung <laurannie02@naver.com>
2024-09-21 12:06:06 +0900

    Title: 정수 명령 처리 5, Time: 228ms, Memory: 28MB, Status: Passed - CodeTree

--- 240921/정수 명령 처리 5/process-numeric-commands-5.py
+++ 240921/정수 명령 처리 5/process-numeric-commands-5.py
@@ -1,14 +1,23 @@
-k = []  # 리스트 초기화
+# 동적 배열을 위한 리스트 초기화
+arr = []
 
+# 명령의 개수 입력
+n = int(input())
 
-a = input().split()  # 입력을 공백 기준으로 나누어 처리
+# 명령어를 처리하는 루프
+for _ in range(n):
+    command = input().split()  # 명령어를 공백으로 구분하여 입력 받음
 
-if a[0] == "push_back":
-    k.append(int(a[1]))  # 두 번째 요소를 리스트에 추가
-elif a[0] == "pop_back":
-    print(k.pop())  # 마지막 요소를 제거하고 출력
-elif a[0] == "get":
-    index = int(a[1])
-    print(k[index])  # 해당 인덱스의 값을 출력
-else:
-    print(k.append(a))
+    if command[0] == "push_back":
+        # push_back 명령일 때, 두 번째 인자가 배열에 추가됨
+        arr.append(int(command[1]))
+    elif command[0] == "pop_back":
+        # pop_back 명령일 때, 배열의 마지막 요소 제거
+        arr.pop()
+    elif command[0] == "size":
+        # size 명령일 때, 배열의 크기 출력
+        print(len(arr))
+    elif command[0] == "get":
+        # get 명령일 때, k번째 요소 출력 (1-based index)
+        k = int(command[1])
+        print(arr[k-1])  # 1-based index에서 0-based index로 변환하여 출력
