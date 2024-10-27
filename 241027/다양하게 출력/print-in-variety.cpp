f622939b7ce59bb61326ca0a175e8b779d7bcb98
Jiyoung <laurannie02@naver.com>
2024-10-27 11:40:15 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241027/다양하게 출력/print-in-variety.cpp
+++ 241027/다양하게 출력/print-in-variety.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main() {
-    cout << "Total days in Year" << endl;
+    cout << "Total days in Year";
     cout << "\n";
-    cout << 365 << endl;
+    cout << 365;
     cout << "\n";
     cout << "Circumference rate" << "\n";
     printf("%.10f\n", 3.1415926535); // printf로 소수점 이하 10자리 출력
