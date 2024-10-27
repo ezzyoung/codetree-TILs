3a3bf7d43390618008c5d819974a808ddc25fbba
Jiyoung <laurannie02@naver.com>
2024-10-27 11:39:29 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241027/다양하게 출력/print-in-variety.cpp
+++ 241027/다양하게 출력/print-in-variety.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <cstdio> // printf 사용을 위해 추가
+
 using namespace std;
 
 int main() {
-    cout<<"Total days in Year";
-    cout<<"\n" ; 
-    cout<<365;
-    cout<<"\n";
-    cout<<"Circumference rate" << "\n" ;
-    cout << 3.1415926535;
+    cout << "Total days in Year" << endl;
+    cout << "\n";
+    cout << 365 << endl;
+    cout << "\n";
+    cout << "Circumference rate" << "\n";
+    printf("%.10f\n", 3.1415926535); // printf로 소수점 이하 10자리 출력
     return 0;
 }
